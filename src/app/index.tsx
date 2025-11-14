ae7a56717bd5aea3a9d27520dbdd6bdf835a0de5
ifiwereabirdiwouldbe19 <167403761+ifiwereabirdiwouldbe19@users.noreply.github.com>
2025-11-14 12:13:16 +0900

    Install expo router

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import {  Text, View } from "react-native";
+
+const  Index = (): JSX.Element => {
+  return (
+    <View>
+      <View >
+        <Text >Hello World</Text>
+      </View>
+    </View>
+  );
+}
+export default Index
