112e7769b867fb275de49f87eafd2388bc6395f9
ifiwereabirdiwouldbe19 <167403761+ifiwereabirdiwouldbe19@users.noreply.github.com>
2025-11-22 11:40:30 +0900

    detail

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,30 +1,44 @@
 import { View, Text, ScrollView, StyleSheet } from "react-native";
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import Icon from "../../components/icon";
+import { onSnapshot,doc } from "firebase/firestore";
+import { useEffect,useState } from "react";
 
 import Circlebutton from "../../components/circleButton";
+import { auth,db} from "../../config";
+import { type Memo } from "../../../types/memo";
 
 const handlePress=():void=>{
     router.push('/memo/edit');
 }
 
 const Detail = (): JSX.Element => {
+    const {id} = useLocalSearchParams();
+    console.log(id);
+    const [memo,setMemo]=useState<Memo|null>(null);
+
+    useEffect(()=>{
+        if(auth.currentUser === null){return;}
+        const ref = doc(db,`users/${auth.currentUser.uid}/memos`,String(id));
+        const unsubscribe = onSnapshot(ref,(memoDoc)=>{
+            const {bodyText,updatedAt} = memoDoc.data() as Memo
+            setMemo({
+                id:memoDoc.id,
+                bodyText,
+                updatedAt
+            }); 
+        })
+        return unsubscribe;
+    },[])
     return(
         <View style={styles.container}>
             <View style={styles.memoHeader}>
-                <Text style={styles.memoTitle}>買い物リスト</Text>
-                <Text style={styles.memoDate}>2025年10月1日 10:00</Text>
+                <Text style={styles.memoTitle} numberOfLines={1}>{memo?.bodyText}</Text>
+                <Text style={styles.memoDate}>{memo?.updatedAt?.toDate().toLocaleString('ja-JP')}</Text>
             </View>
             <ScrollView style={styles.memoBody}>
                 <Text style={styles.memoBodyText}>
-                    Lorem, ipsum dolor sit
-                     amet consectetur adipisicing elit. Ducimus 
-                     voluptas,
-                      suscipit at id sit doloremque ea cumque
-                       recusandae sunt?
-                       Harum dignissimos officia corporis 
-                       recusandae magnam quasi
-                        libero perferendis pariatur nulla!
+                    {memo?.bodyText}
                 </Text>
             </ScrollView>
             <Circlebutton onPress={handlePress} style={{ top :60, bottom: "auto" }}>
@@ -62,6 +76,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 27,
     },
     memoBodyText: {
+              paddingVertical: 32,
         fontSize: 16,
         lineHeight: 24,
     }
